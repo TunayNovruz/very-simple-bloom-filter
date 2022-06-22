b4c74c395755bdaa80c2a4bfbf50c3306f4ba5c6
tunaynovruz <tunaynovruz@gmail.com>
2022-06-22 22:29:33 +0400

    new hashing methods added

--- src/main/java/BloomFilter.java
+++ src/main/java/BloomFilter.java
@@ -1,11 +1,13 @@
+import static java.lang.Math.pow;
+
 public class BloomFilter implements IBloomFilter {
 
-    int[] map;
+    boolean[] map;
     int size;
 
     public BloomFilter(int size) {
         this.size = size;
-        map = new int[size];
+        map = new boolean[size];
     }
 
     /**
@@ -13,7 +15,14 @@ public class BloomFilter implements IBloomFilter {
      */
     @Override
     public void add(String word) {
-        map[getHash(word)] = 1;
+
+        if (word == null) {
+            throw new IllegalArgumentException("Null value");
+        }
+        map[h1(word)] = true;
+        map[h2(word)] = true;
+        map[h3(word)] = true;
+        map[h4(word)] = true;
     }
 
     /**
@@ -22,13 +31,40 @@ public class BloomFilter implements IBloomFilter {
      */
     @Override
     public boolean contains(String word) {
-        return map[getHash(word)] > 0;
+        return map[h1(word)] && map[h2(word)] && map[h3(word)] && map[h4(word)];
     }
 
-    private int getHash(String word) {
-        if (word == null){
-            throw new IllegalArgumentException("Null value");
+
+    private int h1(String s) {
+        int hash = 0;
+        for (int i = 0; i < s.length(); i++) {
+            hash = (hash + (s.charAt(i))) % size;
         }
-        return word.hashCode() % size;
+        return hash;
+    }
+
+    int h2(String s) {
+        int hash = 1;
+        for (int i = 0; i < s.length(); i++) {
+            hash = (hash + (int) pow(19, i) * s.charAt(i)) % size;
+        }
+        return hash;
+    }
+
+    int h3(String s) {
+        int hash = 7;
+        for (int i = 0; i < s.length(); i++) {
+            hash = (hash * 31 + s.charAt(i)) % size;
+        }
+        return hash;
+    }
+
+    int h4(String s) {
+        int hash = 3;
+        for (int i = 0; i < s.length(); i++) {
+            hash += hash * 7 + s.charAt(0) * pow(7, i);
+            hash = hash % size;
+        }
+        return hash;
     }
 }
